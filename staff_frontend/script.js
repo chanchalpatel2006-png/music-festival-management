dad3022047fc90986dcc345047cbd1dba8a8e87e
Chanchal <chanchalpatel2006@gmail.com>
2026-09-20 16:50:51 +0530

    test javascript

---
+++ staff_frontend/script.js
@@ -0,0 +1,25 @@
+const artists = [
+    {
+        artist_id: "AR001",
+        artist_name: "The Driver Era",
+        artist_type: "BAND",
+        country: "USA",
+        manager_id: "MN001"
+    },
+
+    {
+        artist_id: "AR002",
+        artist_name: "Sabrina Carpenter",
+        artist_type: "SOLO",
+        country: "USA",
+        manager_id: "MN002"
+    },
+
+    {
+        artist_id: "AR003",
+        artist_name: "Armaan Malik",
+        artist_type: "SOLO",
+        country: "INDIA",
+        manager_id: "MN001"
+    }
+];
